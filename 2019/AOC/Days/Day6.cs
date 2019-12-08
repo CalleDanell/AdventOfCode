d0f90b2d4cd4d326f7f35c66f14fbbb5d72ea14c
Carl Danell <SEDANELLC@tetrapak.com>
2019-12-08 13:19:47 +0100

    Solve day 6

---
+++ 2019/AOC/Days/Day6.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOC.Days
+{
+    public class Day6 : IDay
+    {
+        private const string CenterOfMass = "COM";
+        private const string You = "YOU";
+        private const string Santa = "SAN";
+
+        public async Task<(string, string)> Solve(string day)
+        {
+            var input = await InputHandler.GetInputByLineAsync(day);
+
+            var solarSystem = SetupSolarSystemMap(input);
+
+            var result1 = TotalOrbitsToCom(solarSystem);
+            var orbitsFromSan = PathToComFromSource(Santa, solarSystem);
+            var orbitsFromYou = PathToComFromSource(You, solarSystem);
+            var result2 = orbitsFromSan.Union(orbitsFromYou).Where(x => !orbitsFromYou.Contains(x) || !orbitsFromSan.Contains(x)).ToList();
+            result2.Remove(You);
+            result2.Remove(Santa);
+
+            return (result1.ToString(), result2.Count.ToString());
+        }
+
+        private static int TotalOrbitsToCom(Dictionary<string, string> solarSystem)
+        {
+            var count = 0;
+            foreach (var (_, parent) in solarSystem)
+            {
+                count += OrbitsToCom(parent, solarSystem);
+            }
+
+            return count;
+        }
+
+        private static List<string> PathToComFromSource(string child, Dictionary<string, string> planets)
+        {
+            var path = new List<string> { child };
+            return planets[child].Equals(CenterOfMass) ? path : path.Concat(PathToComFromSource(planets[child], planets)).ToList();
+        }
+
+        private static int OrbitsToCom(string parent, Dictionary<string, string> planets)
+        {
+            var count = 0;
+            if (parent.Equals(CenterOfMass))
+            {
+                count++;
+                return count;
+            }
+
+            count++;
+            return count + OrbitsToCom(planets[parent], planets);
+        }
+
+        private static Dictionary<string, string> SetupSolarSystemMap(IEnumerable<string> input)
+        {
+            var solarSystem = new Dictionary<string, string>();
+            foreach (var orbit in input)
+            {
+                var split = orbit.Split(')');
+                solarSystem.TryAdd(split[1], split[0]);
+            }
+
+            return solarSystem;
+        }
+    }
+}
--- 2019/AOC/Program.cs
+++ 2019/AOC/Program.cs
@@ -15,7 +15,8 @@ namespace AOC
                 {nameof(Day2), await new Day2().Solve(nameof(Day2))},
                 {nameof(Day3), await new Day3().Solve(nameof(Day3))},
                 {nameof(Day4), await new Day4().Solve(nameof(Day4))},
-                {nameof(Day5), await new Day5().Solve(nameof(Day5))}
+                {nameof(Day5), await new Day5().Solve(nameof(Day5))},
+                {nameof(Day6), await new Day6().Solve(nameof(Day6))}
             };
 
             Console.WriteLine("Results:");
