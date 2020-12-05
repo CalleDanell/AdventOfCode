9a8771dacbf6625cc81cc31f783736cb5b98b013
Carl Danell <SEDANELLC@tetrapak.com>
2020-12-05 17:15:06 +0100

    Solve Day5

---
+++ 2020/Days/Day05.cs
@@ -0,0 +1,68 @@
+﻿using Common;
+using Common.Days;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2020.Days
+{
+    public class Day05 : IDay
+    {
+        const int PlaneRows = 128;
+        const int PlaneColumns = 8;
+
+        public async Task<(string, string)> Solve(string day)
+        {
+            var input = await InputHandler.GetInputByLineAsync(day);
+
+            var boardingPasses = input.Select(x => new BoardingPass(x, PlaneRows, PlaneColumns));
+
+            var resultPartOne = boardingPasses.Max(x => x.GetSeatId());
+
+            var missingRow = boardingPasses.GroupBy(x => x.Row).Where(x => x.Count() == 7).FirstOrDefault().Key;
+            var missingColumn = Enumerable.Range(0, PlaneColumns).Except(boardingPasses.Where(x => x.Row == missingRow).Select(x => x.Column)).FirstOrDefault();
+                        
+            var resultPartTwo = missingRow * 8 + missingColumn;
+
+            return (resultPartOne.ToString(), resultPartTwo.ToString());
+        }
+    }
+
+    public class BoardingPass
+    {
+        public BoardingPass(string input, int planeRows, int planeColumns)
+        {
+            Row = FindSeat(new Queue(input.Take(7).ToList()), planeRows);
+            Column = FindSeat(new Queue(input.TakeLast(3).ToList()), planeColumns);
+        }
+
+        public int Row { get; }
+
+        public int Column { get; }
+
+        public int GetSeatId()
+        {
+            return Row * 8 + Column;
+        }
+
+        private int FindSeat(Queue spacePartitioning, int spaces)
+        {
+            var spaceArray = Enumerable.Range(0, spaces).ToArray();
+            while (spacePartitioning.Count > 0)
+            {
+                var back = spaceArray.Skip(spaceArray.Length / 2);
+                var front = spaceArray.Take(spaceArray.Length / 2);
+                var ins = spacePartitioning.Dequeue();
+                if (ins.Equals('F') || ins.Equals('L'))
+                {
+                    spaceArray = front.ToArray();
+                }
+                else if (ins.Equals('B') || ins.Equals('R'))
+                {
+                    spaceArray = back.ToArray();
+                }
+            }
+            return spaceArray[0];
+        }
+    }
+}
--- 2020/Program.cs
+++ 2020/Program.cs
@@ -17,6 +17,7 @@ namespace _2020
             Console.WriteLine($"{nameof(Day02)}: {await new Day02().Solve(nameof(Day02))} { GetExecutionTime(stopWatch) } ms");
             Console.WriteLine($"{nameof(Day03)}: {await new Day03().Solve(nameof(Day03))} { GetExecutionTime(stopWatch) } ms");
             Console.WriteLine($"{nameof(Day04)}: {await new Day04().Solve(nameof(Day04))} { GetExecutionTime(stopWatch) } ms");
+            Console.WriteLine($"{nameof(Day05)}: {await new Day05().Solve(nameof(Day05))} { GetExecutionTime(stopWatch) } ms");
 
             stopWatch.Stop();
         }
