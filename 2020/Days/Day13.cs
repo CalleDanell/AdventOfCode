1037a03e239203e5f4895da03f2c52bc2b127fd6
Carl Danell <SEDANELLC@tetrapak.com>
2020-12-14 21:51:41 +0100

    13b wip & Solve 14a

---
+++ 2020/Days/Day13.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Common;
+using System.Threading.Tasks;
+
+namespace _2020.Days
+{
+    public class Day13 : IDay
+    {
+        public async Task<(string, string, string)> Solve()
+        {
+            var input = await InputHandler.GetInputByLineAsync(nameof(Day13));
+            var inputList = input.ToList();
+            var earliestTimestamp = long.Parse(inputList.ElementAt(0));
+
+            var buses = inputList.ElementAt(1).Split(',')
+                .Select((x, i) => x != "x" ? 
+                    new Bus { Name = int.Parse(x), TimeOffset = i } : null)
+                .Where(x => x != null)
+                .ToList();
+
+            var result1 = FindEarliestBus(buses, earliestTimestamp);
+            //var result2 = FindEarliestTimestamp(buses);
+            var result2 = "It is too slow...";
+
+            return (nameof(Day13), result1.ToString(), result2);
+        }
+
+        private static long FindEarliestTimestamp(IReadOnlyCollection<Bus> buses)
+        {
+            long earliestTimestamp = 147358165332000;
+            var max = buses.Max(x => x.Name - x.TimeOffset);
+            while (true)
+            {
+                if (earliestTimestamp > 0 && buses.All(b => (earliestTimestamp + b.TimeOffset) % b.Name == 0))
+                {
+                    break;
+                }
+
+                earliestTimestamp += max;
+            }
+
+            return earliestTimestamp;
+        }
+
+
+        private static long FindEarliestBus(IReadOnlyCollection<Bus> buses, long earliestTimestamp)
+        {
+            var nextTimestamp = earliestTimestamp;
+            Bus bus;
+            while (true)
+            {
+                bus = buses.FirstOrDefault(b => nextTimestamp % b.Name == 0);
+                if (bus != null)
+                {
+                    break;
+                }
+                nextTimestamp++;
+            }
+
+            return (nextTimestamp - earliestTimestamp) * bus.Name;
+        }
+
+        public class Bus
+        {
+            public int Name { get; set; }
+
+            public int TimeOffset { get; set; }
+        }
+    }
+}
---
+++ 2020/Days/Day14.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Common;
+using System.Threading.Tasks;
+
+namespace _2020.Days
+{
+    public class Day14 : IDay
+    {
+        public async Task<(string, string, string)> Solve()
+        {
+            var input = await InputHandler.GetInputByLineAsync(nameof(Day14));
+            var instructions = new Queue<string>(input.ToList());
+
+            var instructionGroups = GetInstructionGroups(instructions);
+
+            var maskedValues = RunProgram(instructionGroups);
+            
+            var result1 = maskedValues.Select(x => CharArrayTo36Int(x.Value)).Sum();
+            var result2 = string.Empty;
+
+            return (nameof(Day14), result1.ToString(), result2.ToString());
+        }
+
+        private static Dictionary<string, List<(int address, string digits)>> GetInstructionGroups(Queue<string> instructions)
+        {
+            var instructionGroups = new Dictionary<string, List<(int address, string digits)>>();
+            var currentMask = string.Empty;
+            while (instructions.Count > 0)
+            {
+                var ins = instructions.Dequeue();
+                var parts = ins.Split('=');
+                if (ins.StartsWith("mask"))
+                {
+                    currentMask = parts[1].Trim();
+                    instructionGroups.Add(currentMask, new List<(int address, string digits)>());
+                }
+                else
+                {
+                    var address = int.Parse(Regex.Match(parts[0], "(?<=\\[).+?(?=\\])").Value);
+                    var value = Convert.ToString(long.Parse(parts[1]), 2).PadLeft(36, '0');
+                    instructionGroups[currentMask].Add((address, value));
+                }
+            }
+
+            return instructionGroups;
+        }
+
+        private static Dictionary<int, char[]> RunProgram(Dictionary<string, List<(int address, string digits)>> instructionGroups)
+        {
+            var dict = new Dictionary<int, char []>();
+            foreach (var (mask, valueTuples) in instructionGroups)
+            {
+                var maskArray = mask.ToCharArray();
+                foreach (var (address, digits) in valueTuples)
+                {
+                    var digitArray = digits.ToCharArray();
+                    var result = Mask(digitArray, maskArray);
+                    if (dict.ContainsKey(address))
+                    {
+                        dict[address] = result;
+                    }
+                    else
+                    {
+                        dict.Add(address, result);
+                    }
+                }
+            }
+
+            return dict;
+        }
+
+        private static char[] Mask(char[] digitArray, char[] maskArray)
+        {
+            var resultArray = new char[digitArray.Length];
+            for (var i = 0; i < digitArray.Length; i++)
+            {
+                if (!maskArray[i].Equals('X') && !digitArray[i].Equals(maskArray[i]))
+                {
+                    resultArray[i] = maskArray[i];
+                }
+                else
+                {
+                    resultArray[i] = digitArray[i];
+                }
+            }
+
+            return resultArray;
+        }
+
+        private static long CharArrayTo36Int(IEnumerable<char> digits)
+        {
+            long sum = 0;
+            var reverse = digits.Reverse().ToList();
+
+            for (var i = 0; i < reverse.Count; i++)
+            {
+                if (reverse[i].Equals('1'))
+                {
+                    sum += (long) Math.Pow(2, i);
+                }
+            }
+
+            return sum;
+        }
+    }
+}
