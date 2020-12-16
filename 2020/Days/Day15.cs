efab5fcbf12977d85180b00bf8a9241b36a5e338
Carl Danell <SEDANELLC@tetrapak.com>
2020-12-16 16:44:58 +0100

    Solve 15b

---
+++ 2020/Days/Day15.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Common;
+using System.Threading.Tasks;
+
+namespace _2020.Days
+{
+    public class Day15 : IDay
+    {
+        public async Task<(string, string, string)> Solve()
+        {
+            var input = await InputHandler.GetInputByCommaSeparationAsync(nameof(Day15));
+            var enumerable = input.ToList();
+            var numbers1 = enumerable.Select(long.Parse).ToDictionary(x => x, y => new Queue<long>());
+            var numbers2 = enumerable.Select(long.Parse).ToDictionary(x => x, y => new Queue<long>());
+
+            var result1 = PlayTheGame(numbers1, 2020);
+            var result2 = PlayTheGame(numbers2, 30000000);
+
+            return (nameof(Day15), result1.ToString(), result2.ToString());
+        }
+
+        private long PlayTheGame(Dictionary<long, Queue<long>> numbers, int turns)
+        {
+            long spokenNumbers = 0;
+            long lastSpokenNumber = -1;
+
+            // Play the first round 
+            foreach (var (key, _) in numbers)
+            {
+                numbers[key].Enqueue(spokenNumbers);
+                lastSpokenNumber = key;
+                spokenNumbers++;
+            }
+
+            while (spokenNumbers < turns)
+            {
+                var history = numbers[lastSpokenNumber];
+                if (history.Count == 1)
+                {
+                    lastSpokenNumber = 0;
+                }
+                else
+                {
+                    var first = history.Dequeue();
+                    var last = history.Peek();
+                    lastSpokenNumber = last - first;
+                }
+
+                if (numbers.ContainsKey(lastSpokenNumber))
+                {
+                    numbers[lastSpokenNumber].Enqueue(spokenNumbers);
+                }
+                else
+                {
+                    var queue = new Queue<long>();
+                    queue.Enqueue(spokenNumbers);
+                    numbers.Add(lastSpokenNumber, queue);
+                }
+
+                spokenNumbers++;
+            }
+
+            return lastSpokenNumber;
+        }
+    }
+}
--- 2020/Program.cs
+++ 2020/Program.cs
@@ -15,7 +15,7 @@ namespace _2020
                 new Day04(), new Day05(), new Day06(),
                 new Day07(), new Day08(), new Day09(),
                 new Day10(), new Day11(), new Day12(),
-                new Day13(), new Day14()
+                new Day13(), new Day14(), new Day15()
             };
 
             var solver = new Solver();
