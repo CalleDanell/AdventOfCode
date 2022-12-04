82a833a504f8a3e50dfd4985c903c6548d4a4aba
Carl Danell <SEDANELLC@tetrapak.com>
2022-12-04 09:11:14 +0100

    Add day templatye

---
+++ Common/Template/Day00.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Common.Template
+{
+    public class Day00 : IDay
+    {
+        public async Task<(string, string, string)> Solve()
+        {
+            var day = GetType().Name;
+            var input = await InputHandler.GetInputByLineAsync(day);
+
+            int resultPartOne = 0;
+            int resultPartTwo = 1;
+
+            return (day, resultPartOne.ToString(), resultPartTwo.ToString());
+        }
+    }
+}
