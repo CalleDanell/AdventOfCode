9615a9abd30cd3389a5e75256787ce46dca986c2
Carl Danell <SEDANELLC@tetrapak.com>
2022-12-04 08:38:31 +0100

    Solve Day08 2021

--- 2021/Days/Day08.cs
+++ 2021/Days/Day08.cs
@@ -13,67 +13,118 @@ namespace _2021.Days
 
             var displays = input.Select(x => new Display(x)).ToList();
 
-            var uniqueSegments = displays.SelectMany(x => x.Output.Where(s =>
-                s.Digit == 1 ||
-                s.Digit == 4 ||
-                s.Digit == 7 ||
-                s.Digit == 8
+            var uniqueSegments = displays.SelectMany(x => x.OutputCodes.Where(s =>
+                s.Length == 2 ||
+                s.Length == 3 ||
+                s.Length == 4 ||
+                s.Length == 7
                 ));
 
             var resultPartOne = uniqueSegments.Count();
+            var resultPartTwo = displays.Sum(d => d.CalculateOutput());
 
-            var resultPartTwo = 0;
-
-            return (nameof(Day08), resultPartOne.ToString(), resultPartTwo.ToString());
+            return (this.GetType().Name, resultPartOne.ToString(), resultPartTwo.ToString());
         }
     }
 
     public class Display
     {
+        private string[] codes;
+        private string[] outputCodes;
+
         public Display(string input)
         {
             var parts = input.Split('|');
-            SignalPattern = new List<SignalPattern>(parts[0].Split(' ').Select(DecodeSignalPattern).ToList());
-            Output = new List<SignalPattern>(parts[1].Split(' ').Select(DecodeSignalPattern).ToList());
+            codes = DecodeSignalPattern(parts[0]);
+            outputCodes = parts[1].Trim().Split(' ');
         }
 
-        public List<SignalPattern> Output { get; }
-
-        public List<SignalPattern> SignalPattern { get; }
-
-        private SignalPattern DecodeSignalPattern(string pattern)
+        public int CalculateOutput()
         {
-            var digit = 0;
-            switch (pattern.Length)
+            string result = string.Empty;
+            foreach (var output in outputCodes)
             {
-                case 2:
-                    digit = 1;
-                    break;
-                case 3:
-                    digit = 7;
-                    break;
-                case 4:
-                    digit = 4;
-                    break;
-                case 7:
-                    digit = 8;
-                    break;
-//                case 6:
-                    // 
+                for(var i = 0; i < codes.Length; i++)
+                {
+                    var orderedOutput = new string(output.OrderBy(x => x).ToArray());
+                    var orderedCode = new string(codes[i].OrderBy(x => x).ToArray());
+                    if(orderedOutput.Equals(orderedCode))
+                    {
+                        result += i;
+                    }
+                }
             }
 
-            return new SignalPattern
-            {
-                Digit = digit,
-                Pattern = pattern
-            };
+            return int.Parse(result);
         }
-    }
 
-    public class SignalPattern
-    {
-        public int Digit { get; set; }
+        public string[] OutputCodes => outputCodes;
 
-        public string Pattern { get; set; }
+        private string[] DecodeSignalPattern(string pattern)
+        {
+            var codes = new string[10];
+
+            var fiveCountCodeList = new List<string>();
+            var sixCountCodeList = new List<string>();
+
+            foreach (var code in pattern.Split(' '))
+            {
+                switch (code.Length)
+                {
+                    case 2:
+                        codes[1] = code;
+                        break;
+                    case 3:
+                        codes[7] = code;
+                        break;
+                    case 4:
+                        codes[4] = code;
+                        break;
+                    case 7:
+                        codes[8] = code;
+                        break;
+                    case 5: //2,3,5
+                        fiveCountCodeList.Add(code);
+                        break;
+                    case 6: //0,6,9
+                        sixCountCodeList.Add(code);
+                        break;
+                }
+            }
+
+            foreach (var fiveLen in fiveCountCodeList)
+            {
+                if (fiveLen.Except(codes[1]).Count() == 3)
+                {
+                    codes[3] = fiveLen;
+                }
+                else if (codes[8].Except(fiveLen).Except(codes[4]).Count() == 1)
+                {
+                    codes[5] = fiveLen;
+                } 
+                else
+                {
+                    codes[2] = fiveLen;
+                }
+            }
+
+            foreach (var sixLen in sixCountCodeList)
+            {
+                if (sixLen.Except(codes[1]).Count() == 5)
+                {
+                    codes[6] = sixLen;
+                }
+                else if (sixLen.Except(codes[4]).Count() == 2)
+                {
+                    codes[9] = sixLen;
+                }
+                else
+                {
+                    codes[0] = sixLen;
+                }
+            }
+
+            return codes;
+        }
     }
 }
--- 2021/Program.cs
+++ 2021/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using _2021.Days;
 using Common;
 
@@ -11,14 +9,15 @@ namespace _2021
         private static async Task Main()
         {
             var solver = new Solver(10);
-            await solver.Solve(
-                new Day01(), new Day02(), new Day03(),
-                new Day04(), new Day05(), new Day06(),
-                new Day07(), new Day08(), new Day09(),
-                new Day10(), new Day11(), new Day12(),
-                new Day13(), new Day14(), new Day17(),
-                new Day20(), new Day21(), new Day22()
-                );
+            await solver.Solve(new Day08());
+        //    await solver.Solve(
+        //        new Day01(), new Day02(), new Day03(),
+        //        new Day04(), new Day05(), new Day06(),
+        //        new Day07(), new Day08(), new Day09(),
+        //        new Day10(), new Day11(), new Day12(),
+        //        new Day13(), new Day14(), new Day17(),
+        //        new Day20(), new Day21(), new Day22()
+        //        );
         }
     }
 }
