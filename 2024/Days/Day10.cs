783c396818fed7c3b6789b5d1c7e8043e57e7ccd
Carl Danell <devdanell01@gmail.com>
2024-12-10 10:16:39 +0100

    Solve day10 2024

---
+++ 2024/Days/Day10.cs
@@ -0,0 +1,81 @@
+﻿using Common;
+using Common.Coordinates;
+
+namespace _2024.Days
+{
+    public class Day10 : IDay
+    {
+        public async Task<(string, string, string)> Solve()
+        {
+            var day = GetType().Name;
+            var input = await InputHandler.GetInputByLineAsync(day);
+
+            var map = new Dictionary<Coordinate, int>();
+            for (var y = 0; y < input.Count(); y++)
+            {
+                var row = input.ElementAt(y);
+                for (var x = 0; x < row.Length; x++)
+                {
+                    map.Add(new Coordinate(x, y), int.Parse(row[x].ToString()));
+                }
+            }
+
+            var startingPositions = map.Where(x => x.Value.Equals(0)).ToList();
+
+            var uniquePaths = 0;
+            var numberOfEndNodes = 0;
+            foreach (var start in startingPositions)
+            {
+                var paths = new List<Coordinate>();
+                
+                FindPaths(map, start.Key, paths);
+                var endNodes = new HashSet<Coordinate>(paths);
+
+                uniquePaths += paths.Count;
+                numberOfEndNodes += endNodes.Count;
+            }
+
+            var partOne = numberOfEndNodes;
+            var partTwo = uniquePaths;
+
+            return (day, partOne.ToString(), partTwo.ToString());
+        }
+
+
+        private static void FindPaths(Dictionary<Coordinate, int> map, Coordinate current, List<Coordinate> paths)
+        {
+            var currentValue = map[current];
+            if (currentValue == 9)
+            {
+                paths.Add(current);
+                return;
+            }
+
+            var north = map.TryGetValue(current.GetNorth(), out var northValue) ? northValue : -1;
+            var south = map.TryGetValue(current.GetSouth(), out var southValue) ? southValue : -1;
+            var west = map.TryGetValue(current.GetWest(), out var westValue) ? westValue : -1;
+            var east = map.TryGetValue(current.GetEast(), out var eastValue) ? eastValue : -1;
+
+
+            if (north != -1 && (north - currentValue == 1))
+            {
+                FindPaths(map, current.GetNorth(), paths);
+            }
+
+            if (south != -1 && (south - currentValue == 1))
+            {
+                FindPaths(map, current.GetSouth(), paths);
+            }
+
+            if (west != -1 && (west - currentValue == 1))
+            {
+                FindPaths(map, current.GetWest(), paths);
+            }
+
+            if (east != -1 && (east - currentValue == 1))
+            {
+                FindPaths(map, current.GetEast(), paths);
+            }
+        }
+    }
+}
--- 2024/Program.cs
+++ 2024/Program.cs
@@ -2,4 +2,4 @@
 using Common;
 
 var solver = new Solver();
-await solver.Solve(new Day09());
+await solver.Solve(new Day10());
